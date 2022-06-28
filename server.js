d7a9ed3877d43d40c80dc789c480f9b08355541e
Seb <gillisl22@outlook.com>
2022-06-28 01:25:53 -0400

    adding cookie experation

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: { expires: 15000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
