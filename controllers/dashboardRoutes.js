596a7c31e16f1be09b9a5fbbea9a908e71c50292
Seb <gillisl22@outlook.com>
2022-06-28 10:38:10 -0400

    adding auth and dashroute routes

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const sequelize = require('../config/connections');
+const { Post, User, Comment } = require('../models');
+const auth = require('../utils/auth');
+
+router.get('/', auth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: ['id', 'title', 'post_text'],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }, {
+            model: User,
+            attributes: ['username']
+        }]
+    })
+    .then(dbpost => {
+        const post = dbpost.map(post => post.get({ plain: true}));
+        res.render('dashboard', {
+           post,
+           loggedIn: true
+        });
+    })
+    .catch(err => {
+       console.log(err);
+       res.status(500).json(err);
+    });
+});
+
+router.get('/edit/:id', auth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'post_text'],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }, {
+            model: User,
+            attributes: ['username']
+        }]
+    })
+    .then(dbpost => {
+        if (!dbpost) {
+            res.status(404).json({ message: 'id not found'})
+            return;
+        }
+        const post = dbpost.get({ plain: true});
+        res.render('editPost', {
+            post,
+            loggedIn: true
+        })
+    })
+});
+
+module.exports = router;
