4358510f8f1261f1c1731fe0be7cbfe479b6f1a4
Seb <gillisl22@outlook.com>
2022-06-28 00:30:37 -0400

    models post

---
+++ models/post.js
@@ -0,0 +1,37 @@
+const { stubFalse } = require("lodash");
+const { Model, DataTypes } = require("sequelize/types")
+const sequelize = require('../config/connections');
+
+class Post extends Model {}
+
+Post.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    post_text: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+},  {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
